9fa5c5f0e337aca17700730c7f5a4298af80007b
velocirabbit <www.velocirabbit@gmail.com>
2018-05-09 21:08:36 -0500

    all models working
    Debugged both attention models, and tested them on toy datasets. Running
    them on the PTB dataset now.

--- recurrent_attention/recurrent_attention.py
+++ recurrent_attention/recurrent_attention.py
@@ -20,31 +20,51 @@ class RecurrentAttention(nn.Module):
             `alignment` layer (the default will handle that automatically)
         `h_size`: size of the recurrent hidden states
         `out_size`: size of the output vectors
-        `dropout`: dropout rate (chance of dropping)
         `alignment`: a PyTorch module to use as the alignment subnetwork. If
             `None`, defaults to a single-layer, fully-connected, linear network.
             The input to this layer should be of size `[..., in_size+h_size]`
-            (with `in_size` accounting for whether or not the inputs will be
-            made bi-directional), and the output should be logits rather than
-            softmax probabilities and of size `[..., 1]`
+            (with `in_size` accounting for whether or not the inputs will be made
+            bi-directional), and the output should be of size
+            `[..., prealignment_size]`
+        `smooth_align`: if true, passes the alignment vector logits through a
+            sigmoid function immediately before passing them through a softmax.
+            This reduces the effect very large logits have on the computed
+            alignment vector and also makes it easier for the model to consider
+            multiple locations within the sequence as "important"
+        `dropout`: dropout rate (chance of dropping)
+        `align_location`: if `True`, implements location-aware alignment by
+            taking the previous alignment vector at each step and convolving it
+            with a matrix to obtain a location vector
+        `loc_align_size`: the number of vectors to extract for each position/element
+            in the previous alignment vector (i.e. the number of output channels 
+            in the convolution kernel)
+        `loc_align_kernel`: size of the location alignment kernel used to obtain
+            the location alignment vector (i.e. the number of positions in the
+            sequence the kernel looks at during each convolution step). Padding
+            is automatically calculated and added to make sure the convolution
+            output has the same sequence length as the input. This should be an
+            odd integer (and even integer inputs will be increased by 1) since
+            the amount of padding used is `(loc_align_kernel - 1)/2`
         `attention`: a PyTorch module to use as the attention subnetwork. If
             `None`, defaults to a single-layer, fully-connected, linear network.
             The input to this layer should be of size `[..., in_size+h_size+out_size]`,
             and the output should be logits rather than softmax probabilities
             and of size `[..., out_size]`
-        `rnn_type`: type of RNN cell to use. Should either be `'lstm'` or `'gru'`
-            because that's all I've planned for :/
-        `num_rnn_layers`: number of recurrent layers in the recurrent subnetwork
         `attn_act_fn`: type of activation function to apply to the output of the
             attention subnetwork; after activation, this will be fed back into
             recurrent subnetwork. If `None`, a Linear activation is used
+        `rnn_type`: type of RNN cell to use. Should either be `'lstm'` or `'gru'`
+            because that's all I've planned for :/
+        `num_rnn_layers`: number of recurrent layers in the recurrent subnetwork
         `bidirectional`: whether or not the input sequence to the recurrent
             subnetwork should be made bi-directional (if `True`, `in_size` will
             be doubled)
     '''
-    def __init__(self, in_size, h_size, out_size, dropout = 0.1,
-                alignment = None, attention = None, rnn_type = 'lstm',
-                num_rnn_layers = 1, attn_act_fn = 'Softmax', bidirectional = False):
+    def __init__(self, in_size, h_size, out_size, 
+                alignment = None, smooth_align = False, dropout = 0.1,
+                align_location = False, loc_align_size = 1, loc_align_kernel = 1,
+                attention = None, attn_act_fn = 'Softmax', rnn_type = 'lstm',
+                num_rnn_layers = 1, bidirectional = False):
         super(RecurrentAttention, self).__init__()
         # Fix input parameters
         rnn_type = rnn_type.upper()
@@ -54,18 +74,33 @@ class RecurrentAttention(nn.Module):
         # Initialize some basic layers
         self.drop = nn.Dropout(dropout)
         self.softmax = nn.Softmax(0)
+        self.sigmoid = nn.Sigmoid()
 
         ### Initialize subnetworks ###
-        # Alignment subnetwork
-        if alignment is None:
-            alignment = nn.Linear(in_size + h_size, 1)
-        self.alignment = alignment
-
         # Recurrent subnetwork
         rnn = getattr(nn, rnn_type+'Cell')
-        self.rnn_stack = [rnn(in_size + out_size, h_size)]
-        for _ in range(1, num_rnn_layers):
-            self.rnn_stack.append(rnn(h_size, h_size))
+        self.rnn_stack = nn.ModuleList([
+            rnn(in_size + out_size if i == 0 else h_size, h_size)
+            for i in range(num_rnn_layers)
+        ])
+
+        # Alignment subnetwork
+        if align_location:
+            # Calculate the amount of padding that needs to be applied to each
+            # tensor passed in as an input to the location alignment convolution.
+            # The amount of padding added is such that inputs and outputs have
+            # the same sequence length regardless of the kernel size used, and
+            # is given by: (loc_align_kernel - 1)/2
+            if loc_align_kernel % 2 == 0:  # make sure loc_align_kernel is odd
+                loc_align_kernel += 1
+            pad = (loc_align_kernel - 1) // 2
+            self.loc_align = nn.Conv1d(
+                1, loc_align_size, loc_align_kernel, padding = pad
+            )
+        if alignment is None:
+            align_in_size = in_size + h_size + (loc_align_size if align_location else 0)
+            alignment = nn.Linear(align_in_size, 1)
+        self.alignment = alignment
 
         # Attention subnetwork
         if attention is None:
@@ -75,17 +110,25 @@ class RecurrentAttention(nn.Module):
         # Attention output activation (for recurrent inputs)
         if attn_act_fn is not None:
             fn_type = attn_act_fn
-            if fn_type == 'Softmax':
+            if fn_type in ['Softmax', 'LogSoftmax']:
                 attn_act_fn = getattr(nn, fn_type)(-1)
             else:
                 attn_act_fn = getattr(nn, fn_type)()
         self.attn_act_fn = attn_act_fn
         ################################################
+        # For visualization
+        self.save_attn_wts = False
+        self.attn_wts = None
+        self.rnn_states = None
 
         # Save parameters
         self.in_size = in_size
         self.h_size = h_size
         self.out_size = out_size
+        self.align_location = align_location
+        self.loc_align_size = loc_align_size
+        self.loc_align_kernel = loc_align_kernel
+        self.smooth_align = smooth_align
         self.rnn_type = rnn_type
         self.num_rnn_states = num_rnn_states
         self.num_rnn_layers = num_rnn_layers
@@ -127,15 +170,17 @@ class RecurrentAttention(nn.Module):
         rnn_states = [s if len(s) > 1 else s[0] for s in rnn_states]
         # Output vector
         y = Variable(torch.zeros(batch_size, self.out_size))
-        return (y, rnn_states)
+        c = Variable(torch.zeros(batch_size, self.in_size))
+        return (y, c, rnn_states)
 
     # Run a batch of inputs forward through the recurrent attention mechanism
     # input : [seq_len, batch_size, in_size]
     # output: [seq_len, batch_size, out_size]
     def forward(self, inputs, states):
         # Get the recurrent states and the initial output vector
-        y, rnn_states = states
+        y, c, rnn_states = states
         seq_len = inputs.size(0)
+        batch_size = inputs.size(1)
 
         # Create bi-directional inputs
         if self.bidirectional:
@@ -144,37 +189,77 @@ class RecurrentAttention(nn.Module):
             flipped = torch.index_select(inputs, 0, rev_idx)
             inputs = torch.cat([inputs, flipped], dim = -1)                     # [seq_len, batch_size, in_size]
         
+        # If we're going to be aligning the location, need to initialize an
+        # alignment vector of size [seq_len, batch_size, 1] to use
+        if self.align_location:
+            align_wts = Variable(torch.zeros(seq_len, batch_size, 1))
         y_seq = []
+        # For visualization purposes
+        if self.save_attn_wts:
+            all_attn_wts = []
+            all_rnn_states = []
         # Pass each step of the input one at a time
         for _ in range(seq_len):
-            # Get the hidden state of the first RNN layer. size: [batch_size, h_size]
-            s = rnn_states[0] if not isinstance(rnn_states[0], tuple) else rnn_states[0][0]
-            # Get the alignment weights
-            s = (self.drop(s)).expand(seq_len, -1, -1)                          # [seq_len, batch_size, h_size]
-            align_in = torch.cat([inputs, s], -1)                               # [seq_len, batch_size, in_size+h_size]
-            align_logs = self.alignment(align_in)                               # [seq_len, batch_size, 1]
-            align_wts = self.softmax(align_logs)
-            # Get the context vector for this step as a weighted sum of all steps
-            align_wts = align_wts.expand_as(inputs)                             # [seq_len, batch_size, in_size]
-            c = align_wts.mul(inputs).sum(0)  # `sum()` automatically squeezes  # [batch_size, in_size]
-            # Pass the context vector through the recurrent subnetwork
+            # Concatenate y and c to get the input to the RNN this step
             rnn_in = torch.cat([self.drop(c), y], -1)                           # [batch_size, in_size+out_size]
+            # Run this through the recurrent subnetwork
             new_rnn_states = []
             for rnn, rnn_state in zip(self.rnn_stack, rnn_states):
                 new_states = rnn(rnn_in, rnn_state)  # new_states := (new_h, new_c)
                 new_rnn_states.append(new_states)
-                # The hidden state of this cell is the input to the next layer
-                rnn_in = self.drop(new_states[0])                               # [batch_size, h_size]
+                rnn_in = new_states if not isinstance(new_states, tuple) else new_states[0]
+            if self.save_attn_wts:
+                all_rnn_states.append(new_rnn_states)
+            # The hidden state of the last cell is the input to the alignment
+            s = new_states if not isinstance(new_states, tuple) else new_states[0]
+            # Get the alignment weights
+            _s_ = (self.drop(s)).expand(seq_len, -1, -1)                        # [seq_len, batch_size, h_size]
+            align_in = torch.cat([inputs, _s_], -1)                             # [seq_len, batch_size, in_size+h_size]
+            # Convolve location alignment kernel over previous alignment vector
+            if self.align_location:
+                # Reshape align_wts to match what Conv1d requires
+                _align_wts_ = align_wts.permute(1, 2, 0)                        # [batch_size, 1, seq_len]
+                _loc_align_ = self.loc_align(_align_wts_)                       # [batch_size, loc_align_size, seq_len]
+                loc_align = _loc_align_.permute(2, 0, 1)                        # [seq_len, batch_size, loc_align_size]
+                align_in = torch.cat([align_in, loc_align], -1)                 # [seq_len, batch_size, in_size+h_size+loc_align_size]
+            align_logits = self.alignment(align_in)                             # [seq_len, batch_size, 1]
+            # Logit smoothing
+            if self.smooth_align:
+                align_logits = self.sigmoid(align_logits)
+            align_wts = self.softmax(align_logits)                              # [seq_len, batch_size, 1]
+            if self.save_attn_wts:
+                all_attn_wts.append(align_wts.data.clone())
+            # Get the context vector for this step as a weighted sum of all steps
+            c = inputs.mul(align_wts).sum(0)  # `sum()` squeeze by default      # [batch_size, in_size]
             # Get the output vector for this step
-            attn_in = torch.cat([y, rnn_in, c], -1)                             # [batch_size, in_size+h_size+out_size]
+            attn_in = self.drop(torch.cat([y, s, c], -1))                       # [batch_size, out_size+h_size+in_size]
             y = self.attention(attn_in)                                         # [batch_size, out_size]
-            # Append this output to the output sequence
-            y_seq.append(y.expand(1, -1, -1))
             # Apply an activation function to the attention output
             if self.attn_act_fn is not None:
                 y = self.attn_act_fn(y)
+            # Append this output to the output sequence
+            y_seq.append(y.unsqueeze(0))                                        # [1, batch_size, out_size]
         # Concatenate the output sequence into a single tensor
-        outputs = torch.cat(y_seq, 0)
+        outputs = torch.cat(y_seq, 0)                                           # [seq_len, batch_size, out_size]
+        # Concatenate all of the thing we need for visualization
+        if self.save_attn_wts:
+            self.attn_wts = torch.cat(all_attn_wts, -1)
+            if isinstance(rnn_states[0], tuple):
+              self.rnn_states = [
+                  (
+                      torch.cat([  # Hidden states
+                          all_rnn_states[t][i][0].unsqueeze(-1) for t in range(seq_len)
+                      ], -1).data.clone(),
+                      torch.cat([  # Cell states
+                          all_rnn_states[t][i][1].unsqueeze(-1) for t in range(seq_len)
+                      ], -1).data.clone()
+                  ) for i in range(self.num_rnn_layers)
+              ]
+            else:
+              self.rnn_states = [
+                  torch.cat([
+                      all_rnn_states[t][i].unsqueeze(-1) for t in range(seq_len)
+                  ], -1).data.clone() for i in range(self.num_rnn_layers)
+              ]
         # Return the output sequence, last output vector y, and the new RNN states
-        return outputs, (y, new_rnn_states)
-
+        return outputs, (y, c, new_rnn_states)
--- transformer_attention/transformer_attention.py
+++ transformer_attention/transformer_attention.py
@@ -14,6 +14,7 @@ import torch.nn.functional as F
 
 from torch.autograd import Variable
 
+import copy
 import math
 
 # Produces N copies of a module
@@ -68,39 +69,38 @@ class TransformerAttention(nn.Module):
     Inputs:
         `src_vocab`: size of the source vocabulary
         `tgt_vocab`: size of the target vocabulary
-        `N`: number of encoder and decoder layers
-        `d_embed`: embedding (and general model) size
+        `d_model`: embedding (and general model) size
         `d_ff`: size of the position-wise, fully-connected feed-forward layers
         `h`: number of attention heads
+        `N`: number of encoder and decoder layers
         `encode_position`: whether or not to add positional encoding to the inputs
         `pos_enc_max_len`: maximum length of the sequece positions to encode.
             Ignored if `encode_position` is `False`.
         `dropout`: dropout rate (chance of dropping)
     '''
-    def __init__(self, src_vocab, tgt_vocab, N = 6, d_model = 512, d_ff, h = 8,
-                encode_position = True, pos_enc_max_len = 5000, dropout = 0.1):
+    def __init__(self, src_vocab, tgt_vocab, d_model = 512,
+                d_ff = 2048, h = 8, N = 6, encode_position = True,
+                pos_enc_max_len = 5000, dropout = 0.1):
         super(TransformerAttention, self).__init__()
         
         # Initialize some basic layers
         self.dropout = nn.Dropout(dropout)
-        self.relu = nn.ReLU()
         
         # Initialize a positional encoding layer if needed
         if encode_position:
-            self.src_position = PositionalEncoding(d_model, d_ff, dropout, pos_enc_max_len)
-            self.tgt_position = PositionalEncoding(d_model, d_ff, dropout, pos_enc_max_len)
-            
-        # Source and target normalizations
-        self.src_norm = LayerNorm(d_model)
-        self.tgt_norm = LayerNorm(d_model)
+            self.src_position = PositionalEncoding(d_model, dropout, pos_enc_max_len)
+            self.tgt_position = PositionalEncoding(d_model, dropout, pos_enc_max_len)
             
         # Initialize the encoder and decoder layers
-        self.encoder_stack = [
+        self.encoder_stack = nn.ModuleList([
             EncoderLayer(d_model, d_ff, h, dropout) for _ in range(N)
-        ]
-        self.decoder_stack = [
+        ])
+        self.decoder_stack = nn.ModuleList([
             DecoderLayer(d_model, d_ff, h, dropout) for _ in range(N)
-        ]
+        ])
+        # Encoder and decoder normalizations
+        self.enc_norm = LayerNorm(d_model)
+        self.dec_norm = LayerNorm(d_model)
         
         self.src_vocab = src_vocab
         self.tgt_vocab = tgt_vocab
@@ -111,24 +111,25 @@ class TransformerAttention(nn.Module):
         self.encode_position = encode_position
         self.pos_enc_max_len = pos_enc_max_len
         
-    def forward(self, src, tgt, src_mask, tgt_mask):
+    # IN: [batch_size, seq_len, embed_size]
+    # OUT: [batch_size, seq_len, embed_size]
+    def forward(self, src, tgt, src_mask = None, tgt_mask = None):
         if self.encode_position:
             # Apply positional encoding
             src = self.src_position(src)
             tgt = self.tgt_position(tgt)
-        # Normalize source and target
-        src_norm = self.src_norm(src)
-        tgt_norm = self.tgt_norm(tgt)
             
         # Run the input source through the encoder
-        memory = src_norm
+        memory = src
         for encoder in self.encoder_stack:
             memory = encoder(memory, src_mask)
+        memory = self.enc_norm(memory)
             
         # Input the encoder memory to the decoder and run it forward
-        output = tgt_norm
+        output = tgt
         for decoder in self.decoder_stack:
             output = decoder(output, memory, src_mask, tgt_mask)
+        output = self.dec_norm(output)
             
         return output
 
@@ -159,23 +160,25 @@ class EncoderLayer(nn.Module):
         self.size = d_model
         self.d_ff = d_ff
         self.h = h
-        
-    def forward(self, x, mask):
+    
+    # IN: [batch_size, seq_len, embed_size]
+    # OUT: [batch_size, seq_len, embed_size]
+    def forward(self, x, mask = None):
+        # Normalize input
+        x_norm = self.attn_norm(x)
         # Get the output of the multi-headed self-attention mechanism
-        attn_out = self.attn(x, x, x, mask)
+        attn_out = self.attn(x_norm, x_norm, x_norm, mask)
         # Add the residual connections
-        attn_res = x + self.dropout(attn_norm)
-        # Normalize the residual attention output
-        attn_norm = self.attn_norm(attn_res)
+        attn = x + self.dropout(attn_out)
         
+        # Normalize the residual attention output
+        attn_norm = self.attn_norm(attn)
         # Get the output of the position-wise, fully-connected feed-forward network
         ff_out = self.ff(attn_norm)
         # Add the residual connections
-        ff_res = attn_norm + self.dropout(ff_out)
-        # Normalize the residual PositionwiseFeedForward output
-        ff_norm = self.ff_norm(ff_res)
+        ff = attn_norm + self.dropout(ff_out)
         
-        return ff_norm
+        return ff
 
         
 '''
@@ -217,29 +220,32 @@ class DecoderLayer(nn.Module):
         self.d_ff = d_ff
         self.h = h
         
-    def forward(self, x, memory, src_mask, tgt_mask):
+    # IN: [batch_size, seq_len, embed_size]
+    # OUT: [batch_size, seq_len, embed_size]
+    def forward(self, x, memory, src_mask = None, tgt_mask = None):
+        # Normalize input
+        x_norm = self.self_attn_norm(x)
         # Masked self-attention output
-        self_attn_out = self.self_attn(x, x, x, tgt_mask)
+        self_attn_out = self.self_attn(x_norm, x_norm, x_norm, tgt_mask)
         # Add the residual connections
-        self_attn_res = x + self.dropout(self_attn_out)
-        # Normalize output
-        self_attn_norm = self.self_attn_norm(self_attn_res)
+        self_attn = x + self.dropout(self_attn_out)
         
+        
+        # Normalize input
+        self_attn_norm = self.src_attn_norm(self_attn)
         # Source attention output
-        src_attn_out = self.src_attn(self_attn_norm)
+        src_attn_out = self.src_attn(self_attn_norm, memory, memory, src_mask)
         # Add the residual connections
-        src_attn_res = self_attn_norm + self.dropout(src_attn_out)
-        # Normalize output
-        src_attn_norm = self.src_attn_norm(src_attn_res)
+        src_attn = self_attn + self.dropout(src_attn_out)
         
+        # Normalize input
+        src_attn_norm = self.ff_norm(src_attn)
         # Position-wise, fully-connected feed-forward output
-        ff_out = self.ff(src_attn_norm)
+        ff_out = self.ff(src_attn)
         # Add the residual connections
-        ff_res = src_attn_norm + self.dropout(ff_out)
-        # Normalize output
-        ff_norm = self.ff_norm(ff_res)
+        ff = src_attn_norm + self.dropout(ff_out)
         
-        return ff_norm
+        return ff
     
 
 '''
@@ -358,6 +364,10 @@ class PositionalEncoding(nn.Module):
         # `register_buffer()` is used to register buffers that aren't model parameters
         self.register_buffer('pe', pe)
         
+    # IN: [batch_size, seq_len, embed_size]
+    # OUT: [batch_size, seq_len, embed_size]
     def forward(self, x):
         x = x + Variable(self.pe[:, :x.size(1)], requires_grad = False)
         return self.dropout(x)
+        
+        
